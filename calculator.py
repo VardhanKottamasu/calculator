a337b13ac61241a2e074a0103b1fd616636e416c
vardhan.kottamasu <vardhan.kottamasu@npci.org.in>
2024-04-21 00:54:25 +0530

    corrected definitions for all the methods

--- calculator.py
+++ calculator.py
@@ -4,13 +4,13 @@ def add(a,b):
     return a+b
 
 def subtract(a,b):
-    return a+b
+    return a-b
 
 def multiply(a,b):
-    return a+b
+    return a*b
 
 def divide(a,b):
-    return a+b
+    return a/b
 
 def power(base, exponent):
     return base ** exponent

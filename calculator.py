a1ff69d3022bd41dbe0e661cb21e5b2be9c2cb8a
vardhan.kottamasu <vardhan.kottamasu@npci.org.in>
2024-04-21 00:59:17 +0530

    Handled divide by zero check in divide method

--- calculator.py
+++ calculator.py
@@ -9,8 +9,10 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
-def divide(a,b):
-    return a/b
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
 
 def power(base, exponent):
     return base ** exponent
